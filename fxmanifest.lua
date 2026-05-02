779fb62cb0424b5727e3e485ccceea70de6ff430
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 10:41:39 -0500

    chore: added version to manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,29 @@
 fx_version("cerulean")
 games({ "gta5" })
 lua54("yes")
+version("1.0.2")
+repository("https://www.github.com/PulsarFW/pulsar-core")
 server_script("@oxmysql/lib/MySQL.lua")
 client_script("@pulsar-pwnzor/client/check.lua")
 client_scripts({
-	"sh_init.lua",
-	"core/sh_*.lua",
-	"core/cl_*.lua",
-	"exports/cl_*.lua",
+  "sh_init.lua",
+  "core/sh_*.lua",
+  "core/cl_*.lua",
+  "exports/cl_*.lua",
 })
 
 server_scripts({
-	"sh_init.lua",
-	"sv_config.lua",
-	"core/sv_generator.js",
-	"core/sv_regex.js",
-	"core/sh_*.lua",
-	"core/sv_*.lua",
-	"exports/sv_*.lua",
+  "sh_init.lua",
+  "sv_config.lua",
+  "core/sv_generator.js",
+  "core/sv_regex.js",
+  "core/sh_*.lua",
+  "core/sv_*.lua",
+  "exports/sv_*.lua",
 })
 
 files({
-	"weaponanimations.meta",
+  "weaponanimations.meta",
 })
 
 data_file("WEAPONINFO_FILE_PATCH")("weapons.meta")
